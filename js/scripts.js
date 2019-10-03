6f885a2f2a155c4512f2620bfe6b3e5b0c7df85e
argelomnes <argelomnes@gmail.com>
2019-10-04 02:42:31 +0800

    Mobile FAQ section

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,20 @@ function openTab(evt, tabNumber) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById('defaultOpen').click();
+
+/* FAQ Accordion
+--------------------------------------------*/
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
